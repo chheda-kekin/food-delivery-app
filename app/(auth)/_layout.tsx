9ec69712368e555c34244b883de289a93a483d2e
unknown <kekin.chheda@hotmail.com>
2025-07-22 07:51:30 +0530

    Integrating appWrite

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,18 +1,21 @@
-import { Text } from "react-native";
-
-
-
+import { images } from "@/constants";
 import { Slot } from "expo-router";
-import { SafeAreaView } from "react-native-safe-area-context";
+import { Dimensions, Image, ImageBackground, KeyboardAvoidingView, Platform, ScrollView, View } from "react-native";
+// import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
+
+const screenHeight = Dimensions.get("screen").height;
 
 const _Layout = () => {
     return (
-        <SafeAreaView>
-            <Text>
-                Auth Layout
-            </Text>
+        <KeyboardAvoidingView behavior={Platform.OS === "ios"? "padding": "height"}>
+            <ScrollView className="bg-white h-full" keyboardShouldPersistTaps="handled">
+                <View className="relative w-full" style={{ height: screenHeight/2.25 }}>
+                    <ImageBackground source={images.loginGraphic} resizeMode="stretch" className="size-full rounded-b-lg" />
+                    <Image source={images.logo} className="self-center size-48 absolute -bottom-16  z-10" />
+                </View>
+            </ScrollView>
             <Slot />
-        </SafeAreaView>
+        </KeyboardAvoidingView>
     )
 }
 
---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+    endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+    projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+    platform: "com.techventures.fooddelivery",
+    databaseId: "687e759c00396aebf023",
+    userCollectionId: "687e75e600056bd4d4b9" 
+}
